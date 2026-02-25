a8620d8e6af4fce1e0eef3bbb5827a959c51e17b
Spook242 <spook242@gmail.com>
2026-02-25 17:12:31 +0100

    fix: refactor code

--- src/main/java/cat/itacademy/battleship_api/config/OpenApiConfig.java
+++ src/main/java/cat/itacademy/battleship_api/config/OpenApiConfig.java
@@ -8,22 +8,11 @@ import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@OpenAPIDefinition(
-        info = @Info(
-                title = "Battleship API",
-                version = "1.0",
-                description = "Battleship game API documentation"
-        ),
-        // Esto aplica la seguridad por defecto a todos los endpoints
-        security = @SecurityRequirement(name = "bearerAuth")
-)
-// Esto define CÓMO es la seguridad (Tipo JWT)
-@SecurityScheme(
-        name = "bearerAuth",
-        type = SecuritySchemeType.HTTP,
-        scheme = "bearer",
-        bearerFormat = "JWT"
-)
+@OpenAPIDefinition(info = @Info(title = "Battleship API", version = "1.0", description = "Battleship game API documentation"),
+
+        security = @SecurityRequirement(name = "bearerAuth"))
+
+@SecurityScheme(name = "bearerAuth", type = SecuritySchemeType.HTTP, scheme = "bearer", bearerFormat = "JWT")
 public class OpenApiConfig {
-    // No hace falta poner código dentro, las anotaciones hacen la magia
+
 }
