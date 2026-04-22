8b7dbbc87412dbdeded7cab9e3fc678a8a5f48d7
Spook242 <spook242@gmail.com>
2026-04-22 14:33:35 +0200

    fix: refactor

--- src/main/java/cat/itacademy/battleship_api/config/WebSocketConfig.java
+++ src/main/java/cat/itacademy/battleship_api/config/WebSocketConfig.java
@@ -12,17 +12,12 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-
-
         registry.addEndpoint("/ws").setAllowedOriginPatterns("*").withSockJS();
     }
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
-
         registry.enableSimpleBroker("/topic");
-
-
         registry.setApplicationDestinationPrefixes("/app");
     }
 }
